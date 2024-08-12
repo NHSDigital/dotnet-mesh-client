dc33f956e950189e77762e96950cff39e61e5bed
Michael Clayson <michael.clayson1@nhs.net>
2024-08-12 11:04:25 +0100

    chore: line endings

--- application/DotNetMeshClient/NHS.Mesh.Client/Models/MeshResponse.cs
+++ application/DotNetMeshClient/NHS.Mesh.Client/Models/MeshResponse.cs
@@ -1,12 +1,10 @@
 namespace NHS.MESH.Client.Models;
+
 using NHS.MESH.Client.Models;
 
 public class MeshResponse<TSuccess>
 {
     public bool IsSuccessful;
-
     public TSuccess Response;
-
     public APIErrorResponse? Error;
-
 }
